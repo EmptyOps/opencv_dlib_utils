e9361fb7ea338f7a80348d8490cac3ca315af3c0
Hiren <hiren@Hirens-Mac-mini-2.local>
2019-05-11 11:21:05 +0530

    initial commit

--- facial-landmarks/ffmpeg-angle_face.py
+++ facial-landmarks/ffmpeg-angle_face.py
@@ -13,6 +13,8 @@ import argparse
 # Instantiate the parser
 parser = argparse.ArgumentParser(description='a crop utility')
 
+ext = (".avi", ".mp4")
+
 # Argument are :-- shape_predictor_68_face_landmarks.dat
 parser.add_argument('-p', '--shape-predictor', type=str, nargs='?',
     help='path to facial landmark predictor')
@@ -35,17 +37,18 @@ def resize( path ):
 
     for filename in items:
 
-        if (filename.endswith('.mp4')): #or .avi, .mpeg, whatever. 
+        if (filename.endswith(ext)): #or .avi, .mpeg, whatever. 
 
             # outdir = os.path.join( path, filename+"_dir" )
             # os.makedirs( outdir )
-            # os.system( "ffmpeg -i {0} -f image2 -vf fps=fps=1 {1}".format( os.path.join( path, filename ), os.path.join( outdir, "output%d.jpeg" ) ) )
+            # os.system( "ffmpeg -i {0} -f image2 -vf fps=fps=1 {1}".format( os.path.join( path, filename ), os.path.join( path, "output%d.jpeg" ) ) )
 
             os.system( "ffmpeg -i {0} -f image2 -vf fps=fps=1 {1}".format( os.path.join( path, filename ), os.path.join( path, filename+"%d.jpeg" )))     
 
             items1 = os.listdir(path)
             # with os.system(os.path.join( outdir, open(filename+"_dir.csv", 'wb' ))) as file:
             with open(os.path.join( path+"/out", filename+"_dir.csv"), 'wb' ) as file:
+
                 for item in items1:
 
                     if item == '.DS_Store':
@@ -53,8 +56,6 @@ def resize( path ):
 
                     if (item.endswith('.jpeg')):
                       
-                        line = "\""+item+"\";"
-
                         # load the input image, resize it, and convert it to grayscale
                         images = cv2.imread(path+item)
 
@@ -69,6 +70,8 @@ def resize( path ):
 
                         for (i, rect) in enumerate(rects):
 
+                            line = "\""+item+"~"+str(i)+"\";"
+
                             # determine the facial landmarks for the face region, then
                             # convert the facial landmark (x, y)-coordinates to a NumPy
                             # array
@@ -77,34 +80,30 @@ def resize( path ):
 
                             for (x, y) in shape:
                                     
-                                line = line + ";\""+str(x)+"-"+str(y)+"\""
+                                line = line + ";\""+str(x)+"~"+str(y)+"\""
 
                             file.write(line.encode())
                             file.write('\n'.encode()) 
         else:
           
-             for root, dirs, files in os.walk(path+filename, topdown=False):
+              for root, dirs, files in os.walk(path+filename, topdown=False):
 
-                 for name in files:
+                  for name in files:
                      
-                     if (name.endswith(('.mp4','.avi'))): #or .avi, .mpeg, whatever.
+                      if (name.endswith(ext)): #or .avi, .mpeg, whatever.
 
-                         # resA = os.popen("ffmpeg -i " + os.path.join( path+filename, name )+ " -f null -").read()
-                         
-                         # print("START-----"os.popen("ffmpeg -t -frames"+os.path.join( path+filename, name )).read(),"-----STOP")
                          os.system("ffmpeg -i {0} -f image2 -vf fps=fps=1 {1}".format( os.path.join( path+filename, name ), os.path.join(path+filename, name+"%d.jpeg" )))     
 
                          items1 = os.listdir(root+"/")
 
                          with open(os.path.join( path+"/out", name+"_dir.csv"), 'wb' ) as file:
+
                               for item in items1:
 
                                   if item == '.DS_Store':
                                      continue
 
                                   if (item.endswith('.jpeg')):
-                                    
-                                      line = "\""+item+"\";"
 
                                       # load the input image, resize it, and convert it to grayscale
                                       images = cv2.imread(root+"/"+item)
@@ -122,6 +121,8 @@ def resize( path ):
 
                                       for (i, rect) in enumerate(rects):
 
+                                          line = "\""+item+"~" + str(i)+ "\";"
+
                                           # determine the facial landmarks for the face region, then
                                           # convert the facial landmark (x, y)-coordinates to a NumPy
                                           # array
@@ -129,13 +130,13 @@ def resize( path ):
                                           shape = face_utils.shape_to_np(shape)
 
                                           for (x, y) in shape:
-                                                  
-                                              line = line + ";\""+str(x)+"-"+str(y)+"\""
+
+                                              line = line + ";\""+str(x)+"~"+str(y)+"\""
 
                                           file.write(line.encode())
                                           file.write('\n'.encode()) 
-                     else:
-                         continue                     
+                                  else:
+                                        continue                     
                
 for path in paths:
     resize( path )
